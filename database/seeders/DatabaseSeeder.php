7b16a1f67e3a35a2ea32e389d27f40d9b110faac
Raul <raulramirezolmedo@gmail.com>
2023-04-08 08:42:17 -0400

    se finaliza clase 310

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategoriaSeeder::class);
+        $this->call(ProductoSeeder::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/categorias', CategoriaController::class);
+Route::apiResource('/productos', ProductoController::class);
