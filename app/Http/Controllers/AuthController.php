7b44e58c9161cf4b3da53eafebf836fda0808da0
Raul <raulramirezolmedo@gmail.com>
2023-04-09 21:30:26 -0400

    se finaliza la clase 323

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function register(RegistroRequest $request)
     {
         //Validar el registro
-        $data = $request->validate();
+        $data = $request->validated();
+        //Crear el usuario
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
-    public function login(Request $request)
+
+    public function login(LoginRequest $request)
     {
+        $data = $request->validated();
+
+        // Validar Password
+        if (!Auth::attempt($data)) {
+            return response([
+                'errors' => ['El email o el password son incorrectos']
+            ], 422);
+        }
+
+        //Autenticar al usuario
+        $user = Auth::user();
+        
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
+
     public function logout(Request $request)
     {
     }
--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,4 @@ Route::apiResource('/productos', ProductoController::class);
 
 //Autenticacion
 Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
